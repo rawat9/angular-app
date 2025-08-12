882eeaa94ea21d3fd82b1640098ea0eba21749f3
Anurag Rawat <anurag.rawat0201@gmail.com>
2025-08-12 22:30:37 +0100

    remove import

--- libs/button/src/lib/button.ts
+++ libs/button/src/lib/button.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-button',
-  imports: [CommonModule],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
 export class Button {
   /**
-   * Handles the click event of the button
+   * Handles the click event of the button.
    */
   onClickBtn() {
     console.log('Button was clicked again!!!!');
